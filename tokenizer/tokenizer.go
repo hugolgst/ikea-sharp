36bb87f35140e8556f6e783afad98213f4dbf0d0
Hugo Lageneste <hugo@olivia-ai.org>
2020-04-19 19:19:49 +0200

    Begin to create the tokenizer

---
+++ tokenizer/tokenizer.go
@@ -0,0 +1,14 @@
+package tokenizer
+
+// A Token contains the type and the value of a part of code
+type Token struct {
+	Type  string
+	Value string
+}
+
+// Tokenize takes the given code, tokenize it and returns an array of Tokens
+func Tokenize(code string) (tokens []Token) {
+	var currentIndex int
+
+	return
+}
