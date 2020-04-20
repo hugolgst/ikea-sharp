2a4547fa789847db5794d5734f0db5dca0268b6f
Hugo Lageneste <hugo@olivia-ai.org>
2020-04-20 18:07:50 +0200

    Add the tokenizer test

--- tokenizer/tokenizer_test.go
+++ tokenizer/tokenizer_test.go
@@ -1,13 +1,20 @@
 package tokenizer
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestTokenize(t *testing.T) {
-	example := "INNERSKÄR FUNKÖN 213 ÄPPLARÖ FJÄLLBO"
+	example := "INNERSKÄR FUNKÖN SKOGSFIBBLA Hello world SKOGSFIBBLA ÄPPLARÖ FJÄLLBO"
 	tokens := Tokenize(example)
 
-	fmt.Println(tokens)
+	excepted := []string{
+		"NAME", "PARENTHESES", "QUOTES", "NAME", "NAME", "QUOTES", "PARENTHESES", "SEMICOLON",
+	}
+
+	for i, token := range tokens {
+		if excepted[i] != token.Type {
+			t.Errorf("Tokenize() failed, excepted %s got %s.", excepted[i], token.Type)
+		}
+	}
 }
